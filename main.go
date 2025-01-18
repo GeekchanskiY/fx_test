4974a870e88ce9e638bab0ddc28b5fc6be09b2a9
GeekchanskiY <astrovskiy.04@gmail.com>
2025-01-18 19:34:09 +0300

    tests

--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"go.uber.org/fx"
+)
 
 type SampleA struct {
 	Id string
 }
 
+func NewA(s string) SampleA {
+	return SampleA{Id: s}
+}
+
+func CallSampleA(i SampleA) {
+	fmt.Println(i.Id)
+}
+
 func main() {
-	fmt.Println("Hello World")
+	fx.New(
+		fx.Supply("example"),
+		fx.Provide(NewA),
+		fx.Invoke(CallSampleA),
+		Submodule(),
+	).Run()
+}
+
+func Submodule() fx.Option {
+	return fx.Module(
+		"sampleMod",
+		fx.Invoke(CallSampleA), // Gets it from parent module
+	)
 }
